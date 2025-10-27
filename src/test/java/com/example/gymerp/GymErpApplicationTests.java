ecf8a22926886895efe0d8ef94d4a34167e08bc4
YOONSEONGWAN <sirho123@gmail.com>
2025-10-27 19:45:37 +0900

    gymproject commit

---
+++ src/test/java/com/example/gymerp/GymErpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gymerp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymErpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
