000301d6e3e859ffc5ae4a40e51b4671b8f274f9
joyohan <whdygks9131@naver.com>
2025-10-28 17:00:31 +0900

    251028_상품_수정_1

--- src/main/java/com/example/gymerp/dto/SalesItem.java
+++ src/main/java/com/example/gymerp/dto/SalesItem.java
@@ -4,10 +4,10 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import java.time.LocalDateTime;
-
 import org.apache.ibatis.type.Alias;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
 @Alias("SalesItem")
 @Builder
@@ -16,15 +16,20 @@ import org.apache.ibatis.type.Alias;
 @Data
 public class SalesItem {
 
-    private Long itemSalesId;      		// 상품 판매 내역 ID (PK)
-    private String productId;      		// 상품 코드 (FK -> CODEB.codeBId)
-    private String employeeId;     		// 직원 ID (FK -> employee.empNum)
-    private String productName;    		// 상품명
-    private Integer quantity;      		// 수량
-    private Double unitPrice;      		// 단가
-    private Double totalAmount;    		// 총액
-    private String productType;    		// 구분
-    private String status;         		// 상태
-    private LocalDateTime createdAt;    // 등록일
-    private LocalDateTime updatedAt;    // 수정일
+    /** SALES_ITEM */
+    private Long itemSalesId;        // 상품 판매 ID (PK)         
+    private Long productId;          // 실물 상품 고유번호 (FK)     
+    private Long empNum;             // 직원 고유번호 (FK)          
+
+    private String productName;      // 상품명                        
+    private Integer quantity;        // 수량                       
+
+    private BigDecimal unitPrice;    // 단가                         
+    private BigDecimal totalAmount;  // 총액                  
+
+    private String productType;      // 구분                         
+    private String status;           // 상태 (기본값 'ACTIVE')       
+
+    private LocalDateTime createdAt; // 등록일                   
+    private LocalDateTime updatedAt; // 수정일                        
 }
