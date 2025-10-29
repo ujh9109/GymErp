6128cca225faf39cc199cfd888dd5324f8cf6921
jongbok654 <jongbok654@gmail.com>
2025-10-29 12:38:20 +0900

    2025-10-29 박종복 EmpVacation,EmpAttendance dto,dao,mapper 작성

---
+++ src/main/java/com/example/gymerp/controller/EmpVacationController.java
@@ -0,0 +1,12 @@
+package com.example.gymerp.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("v1")
+@RequiredArgsConstructor
+public class EmpVacationController {
+
+}
