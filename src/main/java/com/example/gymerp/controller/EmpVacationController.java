740b628125327664076d2edaf55cb9da06343746
jongbok654 <jongbok654@gmail.com>
2025-10-29 15:03:13 +0900

    2025-10-29 15:03 박종복 push

--- src/main/java/com/example/gymerp/controller/EmpVacationController.java
+++ src/main/java/com/example/gymerp/controller/EmpVacationController.java
@@ -50,7 +50,7 @@ public class EmpVacationController {
         return ResponseEntity.noContent().build();
     }
 
-    //test1
+    //aaa
     // 상태만 변경
     @PatchMapping("/vacations/{vacNum}/state")
     public ResponseEntity<Void> updateVacationState(@PathVariable int vacNum, @RequestParam String state) {
