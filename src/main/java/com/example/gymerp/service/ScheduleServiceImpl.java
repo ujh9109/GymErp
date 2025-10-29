1bd876c6cdfcfd49a6e0e05a507fb2d9ab3f22d2
yoonjaewon <jikim1032@naver.com>
2025-10-29 12:28:04 +0900

    25.10.29

---
+++ src/main/java/com/example/gymerp/service/ScheduleServiceImpl.java
@@ -0,0 +1,72 @@
+package com.example.gymerp.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.gymerp.dto.ScheduleDto;
+import com.example.gymerp.repository.ScheduleDao;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class ScheduleServiceImpl implements ScheduleService {
+
+	// DAO 주입
+	private final ScheduleDao scheduleDao;
+
+	// 전체 스케줄 조회
+	@Override
+	public List<ScheduleDto> getAllSchedules() {
+
+		return scheduleDao.selectAllSchedules();
+	}
+
+	// 특정 날짜 스케줄 조회
+	@Override
+	public List<ScheduleDto> getSchedulesByDate(String date) {
+
+		return scheduleDao.findByDate(date);
+	}
+
+	// 특정 직원 스케줄 조회
+	@Override
+	public List<ScheduleDto> getSchedulesByEmp(int empNum) {
+
+		return scheduleDao.findByEmpNum(empNum);
+	}
+
+	// 단건 스케줄 조회
+	@Override
+	public ScheduleDto getSchedule(int shNum) {
+
+		return scheduleDao.findByShNum(shNum);
+	}
+
+	// 스케줄 등록
+	@Transactional
+	@Override
+	public int addSchedule(ScheduleDto schedule) {
+
+		return scheduleDao.insert(schedule);
+	}
+
+	// 스케줄 수정
+	@Transactional
+	@Override
+	public int updateSchedule(ScheduleDto schedule) {
+
+		return 0;
+	}
+
+	// 스케줄 삭제
+	@Transactional
+	@Override
+	public int deleteSchedule(int shNum) {
+
+		return scheduleDao.delete(shNum);
+	}
+
+}
