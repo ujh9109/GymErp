40cda555f513080093005f38d257d4a1b3751e53
JungSu94 <wjdtn220@naver.com>
2025-10-30 15:00:01 +0900

    멤버 프로필이미지 기능 추가

--- src/main/java/com/example/gymerp/repository/MemberDao.java
+++ src/main/java/com/example/gymerp/repository/MemberDao.java
@@ -14,5 +14,7 @@ public interface MemberDao {
 	public void insert(MemberDto dto); // 회원 등록 
 	public int delete(int memNum); // 회원 삭제 
 	public int update(MemberDto dto); // 회원 전체 수정 
-    
+	
+	// ▼ 추가: 프로필 이미지 수정
+	public int updateProfile(int memNum, String memProfile);
 }
