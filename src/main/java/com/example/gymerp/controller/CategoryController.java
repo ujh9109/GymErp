64e14b6ab21a5694605b935ea4bae05181778e8a
kimhyeonsoo <hyeonsookim2@gmail.com>
2025-10-31 14:39:57 +0900

    product api 수정

--- src/main/java/com/example/gymerp/controller/CategoryController.java
+++ src/main/java/com/example/gymerp/controller/CategoryController.java
@@ -3,6 +3,7 @@ package com.example.gymerp.controller;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,9 +22,9 @@ public class CategoryController {
 	private final CategoryService categoryService;
 
 	//상품 분류 목록 조회
-	@GetMapping("/categories/list")
-	public List<CodeDto> getCategoryList(CodeDto dto) {
-		
+	@GetMapping("/categories/list/{codeAId}")
+	public List<CodeDto> getCategoryList(CodeDto dto, @PathVariable String codeAId) {
+		dto.setCodeAId(codeAId);
 		return categoryService.getAllCodes(dto);
 	}
 	
