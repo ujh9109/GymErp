f6b2040488e1af8926fb79d43dbb85d0bef01a70
jongbok654 <jongbok654@gmail.com>
2025-10-31 16:53:41 +0900

    2025-10-31 16:53 pjb Commit

--- src/main/java/com/example/gymerp/dto/EmpDto.java
+++ src/main/java/com/example/gymerp/dto/EmpDto.java
@@ -4,6 +4,8 @@ import java.sql.Date;
 
 import org.apache.ibatis.type.Alias;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -15,15 +17,24 @@ import lombok.NoArgsConstructor;
 @Builder
 @Alias("EmpDto")
 public class EmpDto {
-	private int empNum;           // 직원 고유번호 (PK)
+	private Integer empNum;           // 직원 고유번호 (PK)
     private String empName;       // 이름
     private String gender;        // 성별
     private String empAddress;    // 주소
-    private Date empBirth;      // 생년월일
+    
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private Date empBirth;        // 생년월일
+    
     private String empPhone;      // 연락처
     private String empEmail;      // 이메일
+    private String password;      // 비밀번호 2025-10-30 13:07 추가
+    
+    @JsonFormat(pattern = "yyyy-MM-dd")
     private Date hireDate;        // 입사일
+    
+    @JsonFormat(pattern = "yyyy-MM-dd")
     private Date fireDate;        // 퇴사(예정)일
+    
     private String profileImage;  // 프로필 사진 파일명
     private String empMemo;       // 메모
     private String role;          // 직급 (EMP, ADMIN)
