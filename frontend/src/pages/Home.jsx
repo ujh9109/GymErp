0e0adaa961dfd7283d641f2e847fdc2e693f13ea
jongbok654 <jongbok654@gmail.com>
2025-10-31 16:40:48 +0900

    2025-10-31 16:40 박종복 vite.config ,, Home.jsx push

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -6,7 +6,7 @@ import { NavLink } from 'react-router-dom';
 function Home() {
     return <>
         
-            <h1>인덱스 페이지 입니다</h1>
+            <h1>박종복 테스트 페이지 입니다</h1>
              <ul>
                 <li>
                     <NavLink to="/EmpVacationList" >휴가 리스트 테스트</NavLink>
