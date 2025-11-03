aed8ed775d2ebbadf0f7b1c0645b9cd163b691c2
junil <pp9636@naver.com>
2025-11-03 10:13:59 +0900

    스케줄 캘린더 코드테이블기반 백엔드 수정

--- src/main/java/com/example/gymerp/service/CategoryService.java
+++ src/main/java/com/example/gymerp/service/CategoryService.java
@@ -7,4 +7,6 @@ import com.example.gymerp.dto.CodeDto;
 public interface CategoryService {
 	public List<CodeDto> getAllCodes(CodeDto dto);
 	public void save(CodeDto dto);
+	 // 일정 코드 목록
+    public List<CodeDto> getScheduleCodes();
 }
