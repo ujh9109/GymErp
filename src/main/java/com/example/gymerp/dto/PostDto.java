5e6dc98f5351aca458277b89efd22a1c3ea4b4c8
JungSu94 <wjdtn220@naver.com>
2025-11-03 11:18:54 +0900

    post 기능 추가

---
+++ src/main/java/com/example/gymerp/dto/PostDto.java
@@ -0,0 +1,21 @@
+package com.example.gymerp.dto;
+
+import java.sql.Timestamp;
+
+import org.apache.ibatis.type.Alias;
+
+import lombok.Data;
+
+
+@Alias("PostDto")
+@Data
+public class PostDto {
+	private int postId;
+    private String postTitle;
+    private String postContent;
+    private String postWriter;
+    private String postPinned;   // 'Y'/'N'
+    private int postViewCnt;
+    private Timestamp postCreatedAt;
+    private Timestamp postUpdatedAt;
+}
