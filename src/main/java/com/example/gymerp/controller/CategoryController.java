60e0858c6699658beebba55a042d4992a0a8ff86
YOONSEONGWAN <sirho123@gmail.com>
2025-11-07 16:32:42 +0900

    카테고리 수정중 .

--- src/main/java/com/example/gymerp/controller/CategoryController.java
+++ src/main/java/com/example/gymerp/controller/CategoryController.java
@@ -1,8 +1,6 @@
 package com.example.gymerp.controller;
 
 import java.util.List;
-import java.util.Map;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
