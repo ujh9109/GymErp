67974535e4d47fe84cc76bff42f6a884e8673a4f
YOONSEONGWAN <sirho123@gmail.com>
2025-11-07 16:32:07 +0900

    카테고리수정중

--- src/main/java/com/example/gymerp/controller/CategoryController.java
+++ src/main/java/com/example/gymerp/controller/CategoryController.java
@@ -1,7 +1,10 @@
 package com.example.gymerp.controller;
 
 import java.util.List;
+import java.util.Map;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
